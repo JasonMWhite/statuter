cb63af9420abd9851e1b9824260c41a4ef1fa77e
Jason White <jason.white@shopify.com>
2016-03-02 04:23:32 -0500

    refactoring block module, adding tests

---
+++ block.py
@@ -0,0 +1,94 @@
+class Character(object):
+    def __init__(self, text, left, right, bottom, top, size=5.0, font='Courier'):
+        self.text = text
+        self.left = left
+        self.right = right
+        self.top = top
+        self.bottom = bottom
+        self.size = size
+        self.font = font
+
+
+class Word(object):
+    def __init__(self, character):
+        self._characters = [character]
+
+    def add_character(self, character):
+        self._characters.append(character)
+
+    @property
+    def text(self):
+        return ''.join([char.text for char in self._characters])
+
+    @property
+    def num_chars(self):
+        return len(self._characters)
+
+    @property
+    def left(self):
+        return min([char.left for char in self._characters])
+
+    @property
+    def right(self):
+        return max([char.right for char in self._characters])
+
+    @property
+    def top(self):
+        return max([char.top for char in self._characters])
+
+    @property
+    def bottom(self):
+        return min([char.bottom for char in self._characters])
+
+    @property
+    def mean_size(self):
+        total_size = sum([char.size for char in self._characters])
+        return total_size / len(self._characters)
+
+    @property
+    def mode_font(self):
+        fonts = {}
+        for char in self._characters:
+            fonts[char.font] = fonts.get(char.font, 0) + 1
+
+        font, _ = sorted(fonts.items(), key=lambda f: (-f[1], f[0]))[0]
+        return font
+
+
+class Line(object):
+    def __init__(self, word):
+        self._words = [word]
+
+    def add_word(self, word):
+        self._words.append(word)
+
+    @property
+    def left(self):
+        return min([word.left for word in self._words])
+
+    @property
+    def right(self):
+        return max([word.right for word in self._words])
+
+    @property
+    def bottom(self):
+        return min([word.bottom for word in self._words])
+
+    @property
+    def top(self):
+        return max([word.top for word in self._words])
+
+    @property
+    def mean_size(self):
+        weighted_size = sum([word.num_chars * word.mean_size for word in self._words])
+        total_size = sum([word.num_chars for word in self._words])
+        return weighted_size / total_size
+
+    @property
+    def mode_font(self):
+        fonts = {}
+        for word in self._words:
+            fonts[word.mode_font] = fonts.get(word.mode_font, 0) + word.num_chars
+
+        font, _ = sorted(fonts.items(), key=lambda f: (-f[1], f[0]))[0]
+        return font
---
+++ tests/test_block.py
@@ -0,0 +1,92 @@
+from block import Character, Word, Line
+import pytest
+
+
+class TestCharacter(object):
+
+    def test_init(self):
+        character = Character('foo', 1.0, 2.0, 10.0, 11.0)
+        assert character.text == 'foo'
+        assert character.left == 1.0
+        assert character.right == 2.0
+        assert character.bottom == 10.0
+        assert character.top == 11.0
+        assert character.size == 5.0
+        assert character.font == 'Courier'
+
+
+class TestWord(object):
+
+    @pytest.fixture
+    def char_f(self):
+        return Character('f', 1.0, 1.1, 10.0, 10.5, size=5.0)
+
+    @pytest.fixture
+    def char_o(self):
+        return Character('o', 1.1, 1.2, 10.01, 10.51, size=5.5)
+
+    @pytest.fixture
+    def simple_word(self, char_f, char_o):
+        w = Word(char_f)
+        w.add_character(char_o)
+        return w
+
+    def test_add_character(self, char_f, char_o, simple_word):
+        assert simple_word._characters == [char_f, char_o]
+
+    def test_text(self, simple_word):
+        assert simple_word.text == 'fo'
+
+    def test_num_chars(self, simple_word):
+        assert simple_word.num_chars == 2
+
+    def test_boundaries(self, simple_word):
+        assert simple_word.left == 1.0
+        assert simple_word.right == 1.2
+        assert simple_word.top == 10.51
+        assert simple_word.bottom == 10.0
+
+    def test_mean_size(self, simple_word):
+        assert simple_word.mean_size == 5.25
+
+    def test_mode_font(self, simple_word):
+        assert simple_word.mode_font == 'Courier'
+
+
+class TestLine(object):
+
+    @pytest.fixture
+    def first_word(self):
+        w = Word(Character('f', 1.0, 1.1, 10.0, 11.0, size=5.0, font='Courier'))
+        w.add_character(Character('o', 1.1, 1.2, 10.0, 11.0, size=5.2, font='Courier'))
+        w.add_character(Character('o', 1.2, 1.3, 10.0, 11.01, size=5.0, font='Arial'))
+        return w
+
+    @pytest.fixture
+    def second_word(self):
+        w = Word(Character('b', 1.4, 1.5, 10.0, 11.0, size=5.0, font='Courier'))
+        w.add_character(Character('a', 1.5, 1.6, 10.0, 11.0, size=5.2, font='Courier'))
+        w.add_character(Character('r', 1.6, 1.7, 10.01, 11.0, size=5.1, font='Arial'))
+        w.add_character(Character('s', 1.7, 1.8, 10.0, 11.0, size=5.2, font='Arial'))
+        return w
+
+    @pytest.fixture
+    def line(self, first_word, second_word):
+        test_line = Line(first_word)
+        test_line.add_word(second_word)
+        return test_line
+
+    def test_add_word(self, line, first_word, second_word):
+        assert line._words == [first_word, second_word]
+
+    def test_boundaries(self, line):
+        assert line.left == 1.0
+        assert line.right == 1.8
+        assert line.top == 11.01
+        assert line.bottom == 10.0
+
+    def test_mean_size(self, line):
+        assert round(line.mean_size, 2) == 5.1
+
+    def test_mode_font(self, line):
+        assert line.mode_font == 'Arial'
